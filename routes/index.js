38255f4c587a3fa6b33cfb64ffc3f12c8dbc8f0e
Lee seongwon <jsw6357@naver.com>
2023-07-17 21:22:28 +0900

    fix:상이한 폴더 변경

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const storesRouter = require('./stores.routes');
 router.use('/stores/', storesRouter);
 
-// const menusRouter = require('./menus.routes');
-// router.use('/menus/', menusRouter);
+const menusRouter = require('./menus.routes');
+router.use('/menus/', menusRouter);
 
-// const ordersRouter = require('./orders.routes');
-// router.use('/orders/', ordersRouter);
+const ordersRouter = require('./orders.routes');
+router.use('/orders/', ordersRouter);
 
-// const reviewsRouter = require('./reviews.routes');
-// router.use('/reviews/', reviewsRouter);
+const reviewsRouter = require('./reviews.routes');
+router.use('/reviews/', reviewsRouter);
 
 module.exports = router;
