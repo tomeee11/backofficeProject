f55b0bf082e7039604200058f32a1d89533ac0fc
hyem00 <hemi1738@naver.com>
2023-07-17 19:32:42 +0900

    feat:Backend store Architecture Pattern complete

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3003;
 
-const OrderRouter = require('./routes/menus.route');
+const router = require('./routes');
 
 app.use(express.json());
 
-app.use('/', OrderRouter);
+app.use('/api', router);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
