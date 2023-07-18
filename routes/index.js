10ab6f436a0eff23a7a463b4de52a76d06e56185
Lee seongwon <jsw6357@naver.com>
2023-07-18 15:34:59 +0900

    인덱스 머지 에러 해결

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,12 @@ router.use('/stores/', storesRouter);
 const menusRouter = require('./menus.routes');
 router.use('/', menusRouter);
 
-<<<<<<< HEAD
-// const ordersRouter = require('./orders.routes');
-// router.use('/orders/', ordersRouter);
-
-=======
 const userRouter = require('./users.route');
 router.use('/', userRouter);
 
 // const ordersRouter = require('./orders.routes');
 // router.use('/orders/', ordersRouter);
 
->>>>>>> dev/users
 // const reviewsRouter = require('./reviews.routes');
 // router.use('/reviews/', reviewsRouter);
 
